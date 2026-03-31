568d0273af29e1254e32e5601143a597cdc303a6
DVDphobia <l1gend@outlook.com>
2026-03-31 01:39:40 +0000

    feat: static network, google oauth, nginx wss, webrtc fix

--- kousa/lib/beef/schemas/user.ex
+++ kousa/lib/beef/schemas/user.ex
@@ -71,8 +71,6 @@ defmodule Beef.Schemas.User do
     field(:githubAccessToken, :string)
     field(:googleAccessToken, :string)
     field(:discordAccessToken, :string)
-    field(:googleId, :string)
-    field(:googleAccessToken, :string)
     field(:displayName, :string)
     field(:avatarUrl, :string)
     field(:bannerUrl, :string)
